058988d03dcf83cc1039cb6d0751a40374efcac3
Malix <101697781+Malix-Floof@users.noreply.github.com>
2025-03-04 12:31:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from config import settings
 class EasyDiscordBot(commands.Bot):
     def __init__(self):
         super().__init__(
-			command_prefix = settings['PREFIX'], 
-			intents = disnake.Intents.all()
-		)
+	    command_prefix = settings['PREFIX'], 
+	    intents = disnake.Intents.all()
+        )
 
     async def on_ready(self):
         print("Запущен!")
