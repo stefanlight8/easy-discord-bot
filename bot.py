834ebd41a105633543a82af0b752470bc95fa637
Malix <101697781+Malix-Floof@users.noreply.github.com>
2025-03-04 12:31:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,16 @@
-import disnake #Импортируем библиотеку "disnake" // Import the "disnake" library
+import disnake
 from disnake.ext import commands
 from config import settings
 
-class TestBot(commands.Bot):
-    def __init__(self, *args,  **kwargs):
-        super().__init__(*args, **kwargs)
+class EasyDiscordBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+			command_prefix = settings['PREFIX'], 
+			intents = disnake.Intents.all()
+		)
 
     async def on_ready(self):
         print("Запущен!")
 
-intents = disnake.Intents.all()
-bot = MyBot(command_prefix=settings['PREFIX'], intents = intents) #Ваш префикс бота (по умолчанию префикс "!") // Your bot prefix (default prefix "!")
-
-@bot.command()
-async def test(ctx):
-	await ctx.send("Привет! // Hello!")
-	print("Тест команда была успешно выполнена // The test command has been executed")
-
- # Токен бота можно получить на https://discord.com/developers/applications // Bot token available at https://discord.com/developers/applications
+bot = EasyDiscordBot()
 bot.run(settings['TOKEN'])
